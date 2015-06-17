c2abd70ffb72acb39c8d90f7d216153913be206d
Juan B Cabral <jbc.develop@gmail.com>
2015-06-17 19:37:48 -0300

    added api to pypy and integrated with master head of six

---
+++ otree_launcher/libs/pypi.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+from . import six
+from .six.moves import urllib
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+INFO_URL_TEMPLATE = six.u("https://pypi.python.org/pypi/{package}/json")
+
+INFO_VERSION_URL_TEMPLATE = six.u(
+    "https://pypi.python.org/pypi/{package}/{version}/json")
+
+
+# =============================================================================
+# FUNCTION
+# =============================================================================
+
+def info(package, version=None):
+    if version is None:
+        url = INFO_URL_TEMPLATE.format(package=package)
+    else:
+        url = INFO_VERSION_URL_TEMPLATE.format(
+            package=package, version=version)
+    import ipdb; ipdb.set_trace()
+    response = urllib.request.urlopen(url)
+    try:
+        info = json.load(response)
+    finally:
+        response.close()
+    return info
+
+
+
+
+
+
+
+
+
+
+
